42beb3700202270ef81ef556326f526135fc77d9
progz <proximin@gmail.com>
2013-04-15 19:28:00 +0200

    added id functionality for element

--- lib/seems_rateable/helpers.rb
+++ lib/seems_rateable/helpers.rb
@@ -1,16 +1,17 @@
 module Helpers
   	def seems_rateable_style
-  		stylesheet_link_tag    "rateable/jRating.jquery"
+  		stylesheet_link_tag "rateable/jRating.jquery"
   	end
   		
-  	def rating_for(obj, opts={:dimension => nil, :static => false}) 	  
+  	def rating_for(obj, opts={:dimension => nil, :static => false, :id => nil}) 	  
   	   begin
   			kls = opts[:dimension].nil? ? obj.average : obj.average(opts[:dimension])
   			avg = kls ? kls.avg : 0
   			content_tag :div, "", "data-average" => avg,
+  			:id => opts[:id],
         :class => "rateable#{opts[:static] ? " jDisabled" : nil}#{current_user ? nil : " jDisabled"}",
         "data-id" => obj.id, "data-kls" => obj.class.name,
-        "data-dimension" => opts[:dimension]#, "data-size" => "small"       
+        "data-dimension" => opts[:dimension]      
   		rescue StandardError => error
   			eval "raise 'Rateable object #{obj.inspect} does not exist'"
   		end

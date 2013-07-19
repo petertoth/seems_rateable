b14121a8e0a4ac9e28157c3864622b32c9a3e4d7
progz <proximin@gmail.com>
2013-07-19 20:03:54 +0200

    delete old files

--- lib/generators/seems_rateable/templates/cached_model.rb
+++
@@ -1,4 +0,0 @@
-class CachedRating < ActiveRecord::Base
-  attr_accessible :avg, :dimension	
-  belongs_to :cacheable, :polymorphic => true 
-end
--- lib/generators/seems_rateable/templates/model.rb
+++
@@ -1,7 +0,0 @@
-class Rate < ActiveRecord::Base
-  belongs_to :rater, :class_name => "<%= file_name.classify %>"
-  belongs_to :rateable, :polymorphic => true
-  
-  attr_accessible :stars, :dimension
-  
-end
